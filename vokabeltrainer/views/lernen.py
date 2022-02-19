80c0d7b94e1e383dc29efa014a861f4c3bc6a5a3
Simon Beyer <simonbeyer79@gmail.com>
2022-02-19 15:49:05 +0100

    lernen formular steht

--- vokabeltrainer/views/lernen.py
+++ vokabeltrainer/views/lernen.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+import json
 from django.views.generic import TemplateView
 
 from vokabeltrainer.models.vokabel_models import Vokabel, VokabelSet
@@ -9,6 +10,9 @@ class LernenView(TemplateView):
     template_name = "vokabeltrainer/lernen_template.html"
     model = Vokabel
     vokabel_set_id = None
+    question = None
+    question_set = None
+    right_wrong = True
 
     def get_context_data(self, **kwargs):
         context = super(LernenView, self).get_context_data(**kwargs)
@@ -17,31 +21,68 @@ class LernenView(TemplateView):
         context['sets'] = VokabelSet.objects.all()
 
         # question_set ermitteln
-        question = None
-        print(f' get: {self.request.GET}')
-        if 'set_id' in self.request.GET:
-            self.vokabel_set_id = self.request.GET['set_id']
-            question_set = Vokabel.get_random(vokabel_set=self.vokabel_set_id)
+        if not self.right_wrong:
+            self.question = self.question_set
         else:
-            question_set = Vokabel.get_random()
-
+            if 'set_id' in self.request.GET:
+                context['act_set_id'] = self.request.GET['set_id']
+                self.vokabel_set_id = self.request.GET['set_id']
+                self.question_set = Vokabel.get_random(vokabel_set=self.vokabel_set_id)
+            else:
+                self.question_set = Vokabel.get_random()
+            # neue Frage erstellen
             if randrange(2):
-                # print('englisch')
-                question = {
-                    'id': question_set.id,
-                    'question': question_set.german,
-                    'hint': question_set.english_description,
-                    'answer': question_set.english,
+                #  englisch
+                self.question = {
+                    'question': self.question_set.german,
+                    'hint': self.question_set.english_description,
+                    'answer': self.question_set.english,
+                    'lang_to_find': 'english',
                 }
             else:
-                print('deutsch')
-                question = {
-                    'id': question_set.id,
-                    'question': question_set.english,
-                    'hint': question_set.english_description,
-                    'answer': question_set.german,
+                # deutsch
+                self.question = {
+                    "question": self.question_set.english,
+                    "hint": self.question_set.english_description,
+                    "answer": self.question_set.german,
+                    "lang_to_find": "german",
                 }
 
-        context['question_set'] = question
-        print(context)
+        if self.request.POST and not self.right_wrong:
+            context['truefalse'] = False
+            context['truefalse_text'] = "FALSCHE Antwort! Nochmal versuchen!"
+        elif self.request.POST and self.right_wrong:
+            context['truefalse'] = True
+            context['truefalse_text'] = "RICHTIGE Antwort! Du bist super!"
+        else:
+            context['truefalse'] = False
+            context['truefalse_text'] = ''
+
+        context['question_set'] = self.question
         return context
+
+    def post(self, request, *args, **kwargs):
+        # Your code here
+        # Here request.POST is the same as self.request.POST
+        # You can also access all possible self variables
+        # like changing the template name for instance
+
+        # single_quotes to double to make a json string from input
+        question_set = self.request.POST['question_set'].replace("'", '"')
+        print(question_set)
+        question_set = json.loads(question_set)
+
+        correct_answers = question_set['answer'].replace(' ', '').split(',')
+        if self.request.POST['answer'].replace(' ', '') in correct_answers:
+            self.right_wrong = True
+            print('right')
+        else:
+            self.right_wrong = False
+            self.question_set = question_set
+            print('wrong')
+
+        context = self.get_context_data(**kwargs)
+
+        # context['new_variable'] = 'new_variable' + ' updated'
+
+        return self.render_to_response(context)
