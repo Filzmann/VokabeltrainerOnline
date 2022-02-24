ff35e62ec803bb89d05f9b3071024f94cc1f96e9
Simon Beyer <simonbeyer79@gmail.com>
2022-02-24 13:09:35 +0100

    model mit abstract class model InFormal angelegt

--- vokabeltrainer/models/vokabel_models.py
+++ vokabeltrainer/models/vokabel_models.py
@@ -35,14 +35,34 @@ class LobUndAufmunterung(Model):
         return random.sample(liste, 1)[0]
 
 
-class Vokabel(Model):
+class AbstractQuestion(Model):
+    correct_answers = IntegerField(default=0)
+    wrong_answers = IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_random(cls, query=None):
+        if not query:
+            query = cls.objects.all()
+        query = query.annotate(
+            rank=F('wrong_answers') - F('correct_answers')
+        ).order_by('-rank')[:10]
+
+        # print(query)
+        liste = list(query)
+        for elem in liste:
+            print(f'{elem.rank} - {elem}')
+        return random.sample(liste, 1)[0]
+
+
+class Vokabel(AbstractQuestion):
     english = CharField(max_length=255)
     german = CharField(max_length=255)
     english_description = HTMLField(default='')
     example_sentences = HTMLField(default='')
     vokabel_sets = ManyToManyField(to=VokabelSet, related_name='vokabeln', null=True, blank=True)
-    correct_answers = IntegerField(default=0)
-    wrong_answers = IntegerField(default=0)
 
     def __str__(self):
         return f'{self.english} - {self.german}'
@@ -52,25 +72,15 @@ class Vokabel(Model):
         if vokabel_set:
             query = cls.objects.filter(vokabel_sets=vokabel_set)
         else:
-            query = cls.objects.all()
-        query = query.annotate(
-                rank=F('wrong_answers')-F('correct_answers')
-            ).order_by('-rank')[:10]
-
-        # print(query)
-        liste = list(query)
-        for elem in liste:
-            print(f'{elem.rank} - {elem}')
-        return random.sample(liste, 1)[0]
-
-    def add_correct(self):
-        print(f'add_right {self}')
-        self.correct_answers += 1
-        self.save()
-
-    def add_wrong(self):
-        print(f'add_wrong {self}')
-        self.wrong_answers += 1
-        self.save()
+            query = None
+        return super(Vokabel, cls).get_random(query=query)
 
 
+class InFormal(AbstractQuestion):
+    formal = CharField(max_length=255)
+    informal = CharField(max_length=255)
+    example_informal = HTMLField(default='')
+    example_formal = HTMLField(default='')
+
+    def __str__(self):
+        return f'{self.formal} - {self.informal}'
